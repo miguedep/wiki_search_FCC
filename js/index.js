7afada964586a7f5ad80e7da28499bbdea63c91d
Miguel Sanchez <miguedep@gmail.com>
2016-04-30 00:10:48 +0100

    all working No comments

--- js/index.js
+++ js/index.js
@@ -20,8 +20,13 @@ var wikisearch = {
             for (var re in obj){
                x = obj[re];
                console.log(x);
-               $("#results").append("<a href='https://en.wikipedia.org/?curid="+x.pageid+"' target='_blank'><li><h2>"+x.title+"<span id='wikilink'>Link to Wikipedia</span></h2><h3>"+x.extract+"</h3></li></a>");
+               $("#results").append("<a href='https://en.wikipedia.org/?curid="+x.pageid+"' target='_blank'><li><h2>"+x.title+"<span id='wikilink'>Link to Wikipedia <span class='glyphicon glyphicon-globe'></span></span></h2><h3>"+x.extract+"</h3></li></a>");
             }
+            $("li").hover(function(){
+               $(this).addClass("visible");
+            }, function(){
+               $(this).removeClass("visible");
+            });
          },
          error: function(request, errorType, errorMessage) {
             alert('Error: ' + errorType + ' with message: ' + errorMessage);
@@ -48,10 +53,5 @@ $(document).ready(function() {
       value = value.replace(/\s/g, '+');
       $('.ticket').html(value);
       wikisearch.loadWiki(value);
-   });
-   $("li").hover(function(){
-      $("#wikilink").css("display", "none");
-   }, function(){
-      $("#wikilink").css("display", "none");
    });
 });
